21971e4e3ffde2b452c19638f934244c974fb671
rishav <rishavb518@gmail.com>
2025-03-26 18:03:42 +0530

    updated

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,13 +1,13 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = "tokyonight" }
+M.ui = { theme = "gatekeeper" }
 vim.opt.relativenumber = true
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format { bufnr = args.buf }
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*",
+--   callback = function(args)
+--     require("conform").format { bufnr = args.buf }
+--   end,
+-- })
 
 return M
